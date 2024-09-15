5136a92503191c46ae6cd83f5080e01ac47a17ff
NguyenThong251 <ht01252004@gmail.com>
2024-09-14 19:45:13 -0700

    buoi 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import Home from "./pages/user/Home";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./pages/user/Layout";
+import About from "./pages/user/About";
+import Header from "./components/user/Header";
 function App() {
   return (
-   <div className="">
-
-    <Home/>
-
-   </div>
+    <Router>
+      <Header/>
+      <div>
+        {/* Các route sẽ được đặt trong Switch */}
+        <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/user/Layout.jsx
@@ -0,0 +1,23 @@
+import Resgister from "../../components/user/form/Resgister";
+import CardProduct from "../../components/user/ui/CardProduct";
+
+function Layout(){
+   
+return (
+<>
+<div className="">
+Header
+</div>
+
+
+<div className="">
+Footer
+</div>
+
+</>
+
+)
+
+}
+
+export default Layout;
